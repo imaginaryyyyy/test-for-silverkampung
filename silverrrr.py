cfebf1cab22de0d68dbe9b72d03a34a1f03174e1
imaginaryyyyy <imaginary.biz@gmail.com>
2026-05-17 11:47:51 +0800

    Update silverrrr.py

--- silverrrr.py
+++ silverrrr.py
@@ -64,7 +64,7 @@ elif filters == "Showtimes":
             st.image("https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?q=80&w=1138&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 
             st.write("Showtimes:")
-            st.button(movies[1]["showtimes"], key="movie_2)
+            st.button(movies[1]["showtimes"], key="movie_2")
     if movies[2]["showtimes"] in showtimes_filter: 
         with col3:
             st.subheader(movies[2]["title"])
