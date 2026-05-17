e1fbbfe1ea88b144227e9e853cc0c4afdf1d2946
imaginaryyyyy <imaginary.biz@gmail.com>
2026-05-17 15:47:18 +0800

    Update silverrrr.py

--- silverrrr.py
+++ silverrrr.py
@@ -48,14 +48,14 @@ elif filters == "Showtimes":
     showtimes_filter = st.pills("", options=showtimes, default=showtimes, selection_mode="multi")
     index = 0    
     if showtimes_filter:
-        for i in movies:
-                if movies[i]["showtimes"] in showtimes_flter:
+        for movie in movies:
+                if movies[index]["showtimes"] in showtimes_flter:
                         with col[index]:
-                                st.subheader(movies[i]["title"])
-                                st.caption(movies[i]["desc"])
+                                st.subheader(movies[index]["title"])
+                                st.caption(movies[index]["desc"])
                                 st.image("https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?q=80&w=1138&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 
                                 st.write("Showtimes:")
-                                st.button(movies[i]["showtimes"], key=f"{movies[i]["title"]}, {movies[i]["showtime"]}")
+                                st.button(movies[index]["showtimes"], key=f"{movies[index]["title"]}, {movies[index]["showtime"]}")
                                 index += 1
                                 
