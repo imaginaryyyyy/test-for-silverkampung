6771045ff7b1b8e1441486080ee84fbae9b5d9d2
imaginaryyyyy <imaginary.biz@gmail.com>
2026-06-30 18:01:36 +0800

    Create admin_login.py

---
+++ admin_login.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+with col2:
+    st.title("Admin Terminal")
+    container = st.container(border=True)
+    user_name = container.text_input("Username:")
+    container.write("")
+    password = container.text_input("Password:")
+
+with col3:
+    login = container.button("Login")
+
+if user_name and password and login:
+    #Connect to backend for real authentication
+    if user_name == "mosskin-8" and password == "moss-whale-66": #Testing purposes only, the file.json right now does not include the details
+        st.success('Successfully login!', icon="✅")
+        st.switch_page("pages/silverrrr.py") #My own repo, change later
+    else:
+        st.error(f"Unsuccessful login", icon="🚨")
+        st.error('Your Username or password is incorrect', icon="🚨")
