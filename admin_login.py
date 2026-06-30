0a2e11ad4c0efce5ad900f7cc80e30662cac0c0e
imaginaryyyyy <imaginary.biz@gmail.com>
2026-06-30 18:32:12 +0800

    Update admin_login.py

--- admin_login.py
+++ admin_login.py
@@ -3,6 +3,19 @@ import streamlit as st
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
+login_page = st.Page("admin_login.py", title="Login")
+admin_panel = st.Page("silverrrr.py", title="Admin Panel")
+
+def Show_Pages():
+  if st.session_state.logged_in = True
+    pages = [login_page, admin_panel]
+    show_page = st.navigation(pages)
+    show_page.run
+  else:
+    pages = [home_page]
+    show_page = st.navigation(pages)
+    show_page.run
+
 col1, col2, col3 = st.columns(3)
 with col2:
     st.title(":grey[Silver Kampong Admin Terminal]")
@@ -20,7 +33,9 @@ if user_name and password and login:
     if user_name == "mosskin-8" and password == "moss-whale-66": #Testing purposes only, the file.json right now does not include the details
         st.session_state.logged_in = True
         st.success('Successful login!', icon="✅")
-        st.switch_page("pages/silverrrr.py") #My own repo, change later
+        Show_Pages()
+
     else:
         st.error(f"Unsuccessful login", icon="🚨")
         st.error('Your Username or password is incorrect', icon="🚨")
+        Show_Pages()
