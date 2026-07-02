13d1d3be7ed17f5d3d5f59a5105aa4dce9b17557
imaginaryyyyy <imaginary.biz@gmail.com>
2026-07-02 17:24:19 +0800

    Update admin_login.py

--- admin_login.py
+++ admin_login.py
@@ -1,19 +1,5 @@
 import streamlit as st
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
-login_page = st.Page("admin_login.py", title="Login")
-admin_panel = st.Page("show_pages/admin_panel.py", title="Admin Panel")
-
-if st.session_state.logged_in:
-  pages = [admin_panel]
-else:
-  pages = [login_page]
-
-page = st.navigation(pages)
-page.run()
-
 
 col1, col2, col3 = st.columns(3)
 with col2:
