f2eb884e2c04032a383e13eae6b3d23e97b57ca7
imaginaryyyyy <imaginary.biz@gmail.com>
2026-07-02 17:57:44 +0800

    Update admin_login.py

--- admin_login.py
+++ admin_login.py
@@ -5,11 +5,10 @@ with col2:
     st.title(":grey[Silver Kampong Admin Terminal]")
     st.write("[insert slogan]")
     with st.form("login_form", border=True):
-      container = st.container(border=True)
-      container.text_input("**Username:**", key="username")
-      container.write("")
-      container.text_input("**Password:**", key="password")
-      login = container.button(":red[**Login**]")
+      user_name = st.text_input("**Username:**", key="username")
+      st.write("")
+      password = st.text_input("**Password:**", key="password")
+      login = st.form_submit_button(":red[**Login**]")
   
 if login:
     #Connect to backend for real authentication
