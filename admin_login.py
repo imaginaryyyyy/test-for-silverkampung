6848c7cdb9831db159fe2a044ffa808171dedcce
imaginaryyyyy <imaginary.biz@gmail.com>
2026-07-02 17:34:54 +0800

    Update admin_login.py

--- admin_login.py
+++ admin_login.py
@@ -5,7 +5,7 @@ with col2:
     st.title(":grey[Silver Kampong Admin Terminal]")
     st.write("[insert slogan]")
     container = st.container(border=True)
-    container.text_input("**Username:**", key="password")
+    container.text_input("**Username:**", key="username")
     container.write("")
     container.text_input("**Password:**", key="password")
 
