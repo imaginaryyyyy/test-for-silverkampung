c7a6c3afe8258af54b7cbf84c845e6c067a18253
imaginaryyyyy <imaginary.biz@gmail.com>
2026-07-02 17:36:24 +0800

    Update admin_login.py

--- admin_login.py
+++ admin_login.py
@@ -14,7 +14,7 @@ with col3:
 
 if login:
     #Connect to backend for real authentication
-    if st.session_state.user_name == "mosskin-8" and st.session_state.password == "moss-whale-66": #Testing purposes only, the file.json right now does not include the details
+    if st.session_state.username == "mosskin-8" and st.session_state.password == "moss-whale-66": #Testing purposes only, the file.json right now does not include the details
         st.session_state.logged_in = True
         st.success('Successful login!', icon="✅")
         st.rerun()
