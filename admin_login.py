a944da8fe87edc889b88b2deb4e5158b9612f0b6
imaginaryyyyy <imaginary.biz@gmail.com>
2026-08-17 18:49:41 +0800

    Update admin_login.py

--- admin_login.py
+++ admin_login.py
@@ -4,7 +4,7 @@ if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
 login_page = st.Page("admin_login.py", title="Login")
-admin_panel = st.Page("admin_panel.py", title="Admin Panel")
+admin_panel = st.Page("show_pages/admin_panel.py", title="Admin Panel")
 pages = [login_page, admin_panel]
 
 def Show_Pages():
