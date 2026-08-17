b6e54790c2f13f3247bf69536e2698dba49c4c6e
imaginaryyyyy <imaginary.biz@gmail.com>
2026-08-17 18:47:39 +0800

    Update admin_login.py

--- admin_login.py
+++ admin_login.py
@@ -3,8 +3,8 @@ import streamlit as st
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
-login_page = st.Page("app.py", title="Login")
-admin_panel = st.Page("auth.py", title="Admin Panel")
+login_page = st.Page("admin_login.py", title="Login")
+admin_panel = st.Page("show_pages/admin_panel.py", title="Admin Panel")
 pages = [login_page, admin_panel]
 
 def Show_Pages():
