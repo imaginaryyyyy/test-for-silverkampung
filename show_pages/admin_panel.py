1c8777f9146dbba3d72bdcc5bd6bfdfbdf0bfc52
imaginaryyyyy <imaginary.biz@gmail.com>
2026-09-09 15:28:47 +0800

    Update admin_panel.py

--- show_pages/admin_panel.py
+++ show_pages/admin_panel.py
@@ -61,9 +61,9 @@ if st.session_state.show_new_movie:
             st.session_state.json = export(st.session_state.movies)
             st.session_state.download = True
 
-        #if st.session_state.loaded:
-                #st.session_state.movies[title] = movie_details
-                #st.session_state.json = json.dumps(st.session_state.movies, indent=4)
+        if st.session_state.loaded:
+                st.session_state.movies[title] = movie_details
+                st.session_state.json = json.dumps(st.session_state.movies, indent=4)
                 #st.download_button(label="Download New JSON", data=st.session_state.json, file_name="file.json", icon=":material/download:") 
                 #st.success(f"{title} has been saved to file.")
         else:
